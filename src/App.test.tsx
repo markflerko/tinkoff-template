fa8fcf870b5be9cb7d89b317f4e696a8e6fde6bb
Mark Flerko <markflerko@gmail.com>
2023-04-22 13:37:51 +0300

    fix(gh-actions): check header

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the app header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/expense tracker/i);
+  expect(headerElement).toBeInTheDocument();
 });
